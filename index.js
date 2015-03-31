b9ffe32ef4ff2f4b5b5a020f4a5ebb8f341db7cb
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-31 18:24:09 +0300

    better-looking test help

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ module.exports = function (gulp, opts) {
       detectHelp = function (tasks) {
         return R.is(Object, R.path(['help', 'help'], tasks))
       },
+      shortCommand = function (str) {
+        var matches = str.match(/^(\.?\/?node_modules\/.bin\/)?(.*)$/)
+        return matches[1] ? matches[2] : str
+      },
       scripts = require(path.join(process.cwd(), 'package.json')).scripts,
       command = (scripts.test && o.withoutNpmRun) ? scripts.test : 'npm test',
       run = require('childish-process')({
@@ -57,7 +61,7 @@ module.exports = function (gulp, opts) {
   }
 
   if (detectHelp(gulp.tasks)) {
-    var help = o.taskHelp || 'A gulp-npm-test task for ' + '`' + command + '`.'
+    var help = o.taskHelp || 'A gulp-npm-test task, using ' + '`' + shortCommand(command) + '`.'
     gulp.task(o.taskName, help, test)
   }
   else gulp.task(o.taskName, test)
